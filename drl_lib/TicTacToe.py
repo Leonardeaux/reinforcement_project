73332fe571e8fdd2faf0a8406db8adc14a9ac6aa
leonardeaux <enzoleonardo@hotmail.fr>
2023-04-30 16:07:52 +0200

    second commit

---
+++ drl_lib/TicTacToe.py
@@ -0,0 +1,78 @@
+import numpy as np
+
+
+class TicTacToe:
+    def __init__(self):
+        self.board = np.zeros((3, 3), dtype=int)
+        self.player = 1
+        self.winner = None
+        self.game_over = False
+        self.moves = 0
+
+    def reset(self):
+        self.board = np.zeros((3, 3), dtype=int)
+        self.player = 1
+        self.winner = None
+        self.game_over = False
+        self.moves = 0
+
+    def get_state(self):
+        return self.board
+
+    def get_winner(self):
+        return self.winner
+
+    def get_game_over(self):
+        return self.game_over
+
+    def get_player(self):
+        return self.player
+
+    def get_moves(self):
+        return self.moves
+
+    def make_move(self, row, col):
+        if self.board[row][col] == 0:
+            self.board[row][col] = self.player
+            self.moves += 1
+            if self.check_winner():
+                self.winner = self.player
+                self.game_over = True
+            elif self.moves == 9:
+                self.game_over = True
+            else:
+                self.player = -self.player
+        else:
+            print("Invalid move")
+
+    def check_winner(self):
+        # Check rows
+        for row in self.board:
+            if sum(row) == 3 or sum(row) == -3:
+                return True
+
+        # Check columns
+        for col in self.board.T:
+            if sum(col) == 3 or sum(col) == -3:
+                return True
+
+        # Check diagonals
+        if self.board[0][0] + self.board[1][1] + self.board[2][2] == 3 or self.board[0][0] + self.board[1][1] + \
+                self.board[2][2] == -3:
+            return True
+        if self.board[0][2] + self.board[1][1] + self.board[2][0] == 3 or self.board[0][2] + self.board[1][1] + \
+                self.board[2][0] == -3:
+            return True
+
+        return False
+
+    def print_board(self):
+        print(self.board)
+
+    def play(self, row, col):
+        self.make_move(row, col)
+        if self.game_over:
+            if self.winner == 1:
+                print("Player 1 wins")
+            if self.winner == -1:
+                print("Player 2 wins")
--- drl_lib/to_do/dynamic_programming.py
+++ drl_lib/to_do/dynamic_programming.py
@@ -1,15 +1,34 @@
+import numpy as np
+
 from ..do_not_touch.mdp_env_wrapper import Env1
 from ..do_not_touch.result_structures import ValueFunction, PolicyAndValueFunction
 
 
-def policy_evaluation_on_line_world() -> ValueFunction:
+def policy_evaluation_on_line_world(S, A, R, p, pi, theta: float = 0.0000001) -> ValueFunction:
     """
     Creates a Line World of 7 cells (leftmost and rightmost are terminal, with -1 and 1 reward respectively)
     Launches a Policy Evaluation Algorithm in order to find the Value Function of a uniform random policy
     Returns the Value function (V(s)) of this policy
     """
-    # TODO
-    pass
+    V = {s: 0 for s in S}
+
+    while True:
+        delta = 0.0
+
+        for s in S:
+            old_v = V[s]
+            total = 0.0
+            for a in A:
+                total_inter = 0.0
+                for s_p in S:
+                    for r in range(len(R)):
+                        total_inter += p(s, a, s_p, r) * (R[r] + 0.99999 * V[s_p])
+                total_inter = pi(s, a) * total_inter
+                total += total_inter
+            V[s] = total
+            delta = max(delta, np.abs(V[s] - old_v))
+        if delta < theta:
+            return V
 
 
 def policy_iteration_on_line_world() -> PolicyAndValueFunction:
